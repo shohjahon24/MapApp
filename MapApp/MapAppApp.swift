f6264a80015aba52fd1f43a1db87c88a2f0a96b0
shohjahon24 <lalkalaka10@gmail.com>
2023-11-22 19:08:45 +0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Macbook Air on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
