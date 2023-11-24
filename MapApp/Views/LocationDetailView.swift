940a5d04e72fa119153fbc9b9dd097d0ef174563
shohjahon24 <lalkalaka10@gmail.com>
2023-11-24 23:04:35 +0500

    Last commit

---
+++ MapApp/Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MapApp
+//
+//  Created by Macbook Air on 24/11/23.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
